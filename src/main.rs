83497b77fb36ef268b3fd47bf8442bbba21f8b85
welbym <welbymoore0816@gmail.com>
2020-04-10 15:27:35 -0500

    Updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::string::String;
+
+fn main(){
+    // Mutable variable that holds input text
+    let mut input = String::new();
+
+    // Prints a prompt to the Terminal
+    println!("Enter your text:");
+
+    // Takes input from Terminal and stores number of bytes
+    let bytes = io::stdin().read_line(&mut input).unwrap();
+
+    // Number of total characters
+    let characters = bytes - 1;
+
+    let _iter = input.split_whitespace();
+
+    // println!("You typed: {}", input.trim());
+    println!("Number of words read: {}", _iter.count());
+    println!("Number of characters read: {}", characters);
+    // println!("Number of bytes read: {}", bytes)
 }
